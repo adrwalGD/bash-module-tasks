fc4057e1eb64684da26ae0937f406d26449a51a6
Aleksander Drwal <adrwal@griddynamics.com>
2024-10-25 18:07:15 +0200

    made shellcheck happy with quotes

--- task-6/source.sh
+++ task-6/source.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-echo Date: $(date)
-echo User: $USER
-echo Hostname: $(hostname)
-echo Private IP: $(hostname -I | awk '{print $1}')
-echo Public IP: $(curl -s ifconfig.me)
+echo Date: "$(date)"
+echo User: "$USER"
+echo Hostname: "$(hostname)"
+echo Private IP: "$(hostname -I | awk '{print $1}')"
+echo Public IP: "$(curl -s ifconfig.me)"
 source /etc/os-release
-echo OS Name: $NAME
-echo OS Version: $VERSION
-echo Uptime: $(uptime -p)
-echo Used Space: $(df -h --total | tail -n 1 | awk '{print $3}')
-echo Free Space: $(df -h --total | tail -n 1 | awk '{print $4}')
-echo Total Ram: $(free -h | grep Mem | awk '{print $2}')
-echo Free Ram: $(free -h | grep Mem | awk '{print $4}')
-echo Cores Count: $(nproc)
-echo Cores Frequencies: $(cat /proc/cpuinfo | grep "MHz")
+echo OS Name: "$NAME"
+echo OS Version: "$VERSION"
+echo Uptime: "$(uptime -p)"
+echo Used Space: "$(df -h --total | tail -n 1 | awk '{print $3}')"
+echo Free Space: "$(df -h --total | tail -n 1 | awk '{print $4}')"
+echo Total Ram: "$(free -h | grep Mem | awk '{print $2}')"
+echo Free Ram: "$(free -h | grep Mem | awk '{print $4}')"
+echo Cores Count: "$(nproc)"
+echo Cores Frequencies: "$(cat /proc/cpuinfo | grep "MHz")"
